93cd50946e60f61c1377990f521e9c6b5b7a20e4
James Issac <jissac@users.noreply.github.com>
2017-06-21 14:18:06 -0400

    Add files via upload

--- Titanic/titanic_analysis.py
+++ Titanic/titanic_analysis.py
@@ -18,6 +18,7 @@ from sklearn.linear_model import Perceptron
 from sklearn.linear_model import SGDClassifier
 from sklearn.tree import DecisionTreeClassifier
 
+#######################   Preparing the Data   ##############3######################
 ### Load training and test data
 #train_df = pd.read_csv('/Users/JI/Documents/Projects/Visualizations/train.csv')
 #test_df = pd.read_csv('/Users/JI/Documents/Projects/Visualizations/test.csv')
@@ -27,6 +28,10 @@ combine = [train_df, test_df]
 
 # preview data
 train_df.head(10)
+
+# find any null columns
+train_df.isnull().any()
+test_df.isnull().any()
 
 # size of data
 train_df.shape
@@ -153,7 +158,156 @@ for dataset in combine:
     dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
 train_df[['IsAlone', 'Survived']].groupby(['IsAlone'], as_index=False).mean()
 
+train_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1, inplace=True)
+test_df.drop(['Parch', 'SibSp', 'FamilySize'], axis=1, inplace =True)
+combine = [train_df, test_df]
+
+train_df.head()
+
 # artificial feature combining Pclass and Age
 for dataset in combine:
     dataset['Age*Class'] = dataset.Age * dataset.Pclass
 train_df.loc[:, ['Age*Class', 'Age', 'Pclass']].head(10)
+
+freq_port = train_df.Embarked.dropna().mode()[0]
+freq_port
+
+for dataset in combine:
+    dataset['Embarked'] = dataset['Embarked'].fillna(freq_port)
+    
+train_df[['Embarked', 'Survived']].groupby(['Embarked'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+
+for dataset in combine:
+    dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+
+train_df.head()
+
+test_df['Fare'].fillna(test_df['Fare'].dropna().mean(), inplace=True)
+test_df.head()
+
+train_df['FareBand'] = pd.qcut(train_df['Fare'], 4)
+train_df[['FareBand', 'Survived']].groupby(['FareBand'], as_index=False).mean().sort_values(by='FareBand', ascending=True)
+
+for dataset in combine:
+    dataset.loc[ dataset['Fare'] <= 7.91, 'Fare'] = 0
+    dataset.loc[ (dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+    dataset.loc[ (dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare'] = 2
+    dataset.loc[ dataset['Fare'] > 31 , 'Fare'] = 3
+    #dataset['Fare'] = dataset['Fare'].astype(int)
+
+train_df.drop(['FareBand'], axis=1, inplace=True)
+combine = [train_df, test_df]
+
+train_df.head()
+
+#######################   Machine Learning  ##############3######################3
+X_train = train_df.drop('Survived', axis=1)
+Y_train = train_df['Survived']
+X_test = test_df.drop('PassengerId', axis=1).copy()
+X_train.shape, Y_train.shape, X_test.shape
+
+# Logistic Regression
+logreg = LogisticRegression(solver='newton-cg', multi_class='multinomial')
+logreg.fit(X_train, Y_train)
+Y_pred = logreg.predict(X_test)
+acc_log = round(logreg.score(X_train, Y_train) * 100, 2)
+acc_log
+
+coeff_df = pd.DataFrame(train_df.columns.delete(0))
+coeff_df.columns = ['Feature']
+coeff_df['Correlation'] = pd.Series(logreg.coef_[0])
+coeff_df.sort_values(by='Correlation', ascending=False)
+
+# SVM
+svc = SVC()
+svc.fit(X_train, Y_train)
+Y_pred = svc.predict(X_test)
+acc_svc = round(svc.score(X_train, Y_train) * 100, 2)
+acc_svc
+
+## KNN
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(X_train, Y_train)
+Y_pred = knn.predict(X_test)
+acc_knn = round(knn.score(X_train, Y_train) * 100, 2)
+acc_knn
+
+# Gaussian Naive Bayes
+gaussian = GaussianNB()
+gaussian.fit(X_train, Y_train)
+Y_pred = gaussian.predict(X_test)
+acc_gaussian = round(gaussian.score(X_train, Y_train) * 100, 2)
+acc_gaussian
+
+# Perceptron
+perceptron = Perceptron()
+perceptron.fit(X_train, Y_train)
+Y_pred = perceptron.predict(X_test)
+acc_perceptron = round(perceptron.score(X_train, Y_train) * 100, 2)
+acc_perceptron
+
+# Linear SVC
+linear_svc = LinearSVC()
+linear_svc.fit(X_train, Y_train)
+Y_pred = linear_svc.predict(X_test)
+acc_linear_svc = round(linear_svc.score(X_train, Y_train) * 100, 2)
+acc_linear_svc
+
+# Stochastic Gradient Descent
+sgd = SGDClassifier()
+sgd.fit(X_train, Y_train)
+Y_pred = sgd.predict(X_test)
+acc_sgd = round(sgd.score(X_train, Y_train) * 100, 2)
+acc_sgd
+
+# Decision Tree
+decision_tree = DecisionTreeClassifier()
+decision_tree.fit(X_train, Y_train)
+Y_pred = decision_tree.predict(X_test)
+acc_decision_tree = round(decision_tree.score(X_train, Y_train) * 100, 2)
+acc_decision_tree
+
+# Random Forest
+random_forest = RandomForestClassifier(n_estimators=100)
+random_forest.fit(X_train, Y_train)
+Y_pred = random_forest.predict(X_test)
+random_forest.score(X_train, Y_train)
+acc_random_forest = round(random_forest.score(X_train, Y_train) * 100, 2)
+acc_random_forest
+
+# Summary of all models
+models = pd.DataFrame({
+    'Model': ['Support Vector Machines', 'KNN', 'Logistic Regression', 
+              'Random Forest', 'Naive Bayes', 'Perceptron', 
+              'Stochastic Gradient Decent', 'Linear SVC', 
+              'Decision Tree'],
+    'Score': [acc_svc, acc_knn, acc_log, 
+              acc_random_forest, acc_gaussian, acc_perceptron, 
+              acc_sgd, acc_linear_svc, acc_decision_tree]})
+models.sort_values(by='Score', ascending=False)
+
+#########################3 Submissions ##################################
+submission = pd.DataFrame({
+        "PassengerId": test_df["PassengerId"],
+        "Survived": Y_pred
+    })
+submission.to_csv('logreg.csv', index=False)
+
+submission = pd.DataFrame({
+        "PassengerId": test_df["PassengerId"],
+        "Survived": Y_pred
+    })
+submission.to_csv('random_forest.csv', index=False)
+
+
+####################
+#for i, row in test_df.iterrows():
+#    if test_df.loc[i,'Fare'] == float('nan'):
+#        print i
+#
+#type(fare[152])
+#type(float('nan'))
+#combine[1]['Fare'].fillna(combine[1]['Fare'].dropna().mean(), inplace=True)
+#for i in combine[1]['Fare']:
+#    if combine[1]['Fare'][i] == 'nan':
+#        print 'yes'
