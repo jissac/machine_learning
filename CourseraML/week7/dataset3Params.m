75096bc2b1211f6041185fc81cb4b59ed715199b
James Issac <jissac@users.noreply.github.com>
2017-11-04 23:38:22 -0400

    Add files via upload

--- CourseraML/week7/dataset3Params.m
+++ CourseraML/week7/dataset3Params.m
@@ -23,12 +23,22 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-
-
-
+C_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
+sigma_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
+min_error = 500;
+for ii=1:length(C_vec)
+  for jj=1:length(sigma_vec)
+    model = svmTrain(X, y, C_vec(ii), @(x1, x2) gaussianKernel(x1, x2, sigma_vec(jj))); 
+    predictions = svmPredict(model,Xval);
+    pred_error = mean(double(predictions ~= yval));
+    if (pred_error < min_error)
+      min_error = pred_error;
+      C = C_vec(ii);
+      sigma = sigma_vec(jj);
+    end  
+  end
+end
 
 
 
 % =========================================================================
-
-end
